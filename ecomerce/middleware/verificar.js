31e1475d8f515a76b9655f1e7e6cb2a53254c40f
alexandrajimenezc <artesalexj@gmail.com>
2020-05-23 20:13:59 +0200

    cambio de verificarToken a verificar en el middleware

---
+++ ecomerce/middleware/verificar.js
@@ -0,0 +1,53 @@
+const jwt = require('jsonwebtoken');
+const config=process.env.secret
+const UserModel= require('../models/userModel');
+
+
+const verificarToken = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        const decodificar = jwt.verify(token, config);
+        const usuario = await UserModel.findOne({
+            _id: decodificar._id,
+            tokens: token
+        });
+        if (!usuario) {
+            return res.status(401).send({
+                message: 'NO ESTAS AUTORIZADO'
+            });
+        }
+        req.usuario = usuario;
+        next();
+    } catch (error) {
+        console.error(error)
+        res.status(401).send({
+            message: 'NO ESTAS AUTORIZADO',
+            error
+        })
+    }
+}
+
+
+/* const is = (roles) => async(req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+        return res.status(403).send({
+            message: 'You are not allowed to access this zone'
+        })
+    }
+    next();
+} */
+
+/* const  isConfirmado =(emaill) => async(req, res, next)=>{
+
+    const usuario = await UserModel.findOne({email:emaill})
+    if(usuario.confirmado !== true){
+        return res.status(403).send({
+            message: 'No ha validado el correo'
+        })
+    }
+    next()
+} */
+ 
+
+
+module.exports = {verificarToken };
